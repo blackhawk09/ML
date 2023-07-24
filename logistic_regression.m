c4a967d57d55b7b07377e325924f47731a2be509
Pragadeesh <sbpragadeesh@gmail.com>
2023-07-24 11:40:08 +0530

    Working now

--- logistic_regression.m
+++ logistic_regression.m
@@ -3,21 +3,20 @@ data = load('binary_classification.txt');
 x = data(:,1);
 x = [x ones(length(x),1)];
 y = data(:,2);
-t = [0;0];
+t = randn(2,1);
 m = length(y);
-a = 0.00000001;
-itr = 1500;
+a = 0.0005;
+itr = 25000;
 J = [];
 plot(x(:,1),y,'or');
 for i=1:itr
     h = 1./(1+exp(-x*t));
     Jc = (1/m)*sum(y.*log(h) + (1-y).*log(1-h));
     J = [J Jc];
-    t1 = t(1) - (1/m)*sum((h-y).*x(:,1));
-    t2 = t(2) - (1/m)*sum(h-y);
+    t1 = t(1) - (a/m)*sum((h-y).*x(:,1));
+    t2 = t(2) - (a/m)*sum(h-y);
     t = [t1;t2];
 end
 hold
-plot(x(:,1),h)
 figure;
 plot(J);
